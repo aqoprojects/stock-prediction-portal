4b3ccacbe0d4afe124975c7e344d359b81d82147
ajao quyum <ajaoquyumolamide@gmail.com>
2025-07-05 17:07:28 +0100

    registration page

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import './assets/css/style.css'
-import Header from './components/Header';
 import Main from './components/Main';
+import {BrowserRouter, Routes, Route} from 'react-router';
+import Register from './components/Register';
+import Header from './components/Header';
 import Footer from './components/Footer';
+import Login from './components/Login';
 
 
 function App() {
@@ -10,9 +13,15 @@ function App() {
 
   return (
     <>
-      <Header/>
-      <Main/>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Main/>} />
+      <Route path='/register' element={<Register/>}/>
+      <Route path="/login" element={<Login/>}/>
+    </Routes>
       <Footer/>
+      </BrowserRouter>
     </>
   )
 }
