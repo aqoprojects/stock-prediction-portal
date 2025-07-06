0ef4f72b8e7b0e90f1144d4eab9e6a6ceed56d61
ajao quyum <ajaoquyumolamide@gmail.com>
2025-07-06 16:34:36 +0100

    loggin functionality

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,29 +1,32 @@
-import { useState } from 'react'
-import './assets/css/style.css'
+import { useState } from 'react';
+import './assets/css/style.css';
 import Main from './components/Main';
-import {BrowserRouter, Routes, Route} from 'react-router';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import Register from './components/Register';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Login from './components/Login';
+import AuthProvider from './AuthProvider';
 
+function App ()
+{
 
-function App() {
- 
 
   return (
     <>
-    <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path='/' element={<Main/>} />
-      <Route path='/register' element={<Register/>}/>
-      <Route path="/login" element={<Login/>}/>
-    </Routes>
-      <Footer/>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Main />} />
+            <Route path='/register' element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -1,8 +1,81 @@
-import React from 'react'
+import { useContext, useState } from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faSpinner} from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
+
+import {useNavigate} from 'react-router-dom'
+import { AuthContext } from '../AuthProvider'
+
+
+
+
 
 const Login = () => {
+  const [username, setUsername] =useState('')
+  const [password, setPassword] =useState('')
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+  const [error,setError] = useState('')
+  const {isLoggedIn, setIsLoggedIn} = useContext(AuthContext)
+
+  const handleLogin = async (e)=>{
+    e.preventDefault();
+    setLoading(true)
+    const userData = {username, password}
+    console.log(userData);
+
+    try{
+      const resposne = await axios.post('http://localhost:8000/api/v1/token/', userData)
+      localStorage.setItem('accessToken', resposne.data.access)
+      localStorage.setItem('refreshToken', resposne.data.refresh)
+      console.log('Login successful');
+      setIsLoggedIn(true)
+      navigate('/')
+    } catch (error) {
+      setError('Invalid credentials')
+    }
+    finally{
+      setLoading(false)
+    }
+  
+  }
+
+
+
   return (
-    <div className='text-light container'>Login</div>
+    <>
+      <div className="container">
+        <div className="row jusify-content-center">
+          <div className="col-md-6 bg-light-dark p-5 rounded">
+
+            <h3 className='text-light text-center mb-4'>Login to our portal</h3>
+            <form onSubmit={handleLogin}>
+              <div className='mb-3'>
+                <input type="text" className='form-control' placeholder='Enter username' value={username} onChange={( e ) => setUsername( e.target.value )} />
+
+                
+              </div>
+
+              <div className='mb-3'>
+                <input type="password" className='form-control' placeholder='Set password' value={password} onChange={( e ) => setPassword( e.target.value )} />
+
+                
+              </div>
+              
+              {error && <div className='text-danger'>{error}</div>}
+
+              
+              {loading ? (
+                <button type="submit" className='btn btn-info d-block mx-auto' disabled> <FontAwesomeIcon icon={faSpinner} spin/>Logging in..</button>) : (
+                  <button type="submit" className='btn btn-info d-block mx-auto'>Register</button>
+                )
+              }
+              
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
